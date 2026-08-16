014c074eb2289eee73304717886731574968090e
pycraft7 <pycraft7@gmail.com>
2026-08-16 21:12:03 +0545

    Profile improvements and host fix

--- config/settings.py
+++ config/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-m+0n-9rx0kli7%$a7hlad6^39s$x_j2$e=h73^phob79b6!&4&")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "False") == "True"
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
+DEBUG = os.environ.get("DEBUG", "True") == "True"
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1,localhost,testserver").split(",")
 
 # Application definition
 
