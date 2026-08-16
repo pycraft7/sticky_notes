81ad6255b60fb2a7d526244d113bf1c4e2b1d8e4
pycraft7 <pycraft7@gmail.com>
2026-08-16 21:26:33 +0545

    Serve media files with explicit URL pattern

--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 from accounts import views as accounts_views
 
 urlpatterns = [
@@ -27,7 +27,10 @@ urlpatterns = [
     path("notes/", include("notes.urls")),
     path("accounts/", include("accounts.urls")),
 ]
-urlpatterns += static(
-    settings.MEDIA_URL,
-    document_root=settings.MEDIA_ROOT
-)
+urlpatterns += [
+    re_path(
+        r"^media/(?P<path>.*)$",
+        serve,
+        {"document_root": settings.MEDIA_ROOT}
+    ),
+]
